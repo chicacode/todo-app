1039cf209c97d6e257d79cb19ae7135dd939cdcb
Geraldine <gerahernan.al@gmail.com>
2023-01-28 20:16:52 -0800

    feat: adding js

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("UI-TODOapp")
+
+const inputBox = document.querySelector(".searchbox input");
+const addBtn = document.querySelector(".button-container-search button");
+const todoList = document.querySelector(".todoList");
+const deleteAllBtn = document.querySelector(".button-task-list__trash");
+
+console.log("search", inputBox.value);
+
+console.log("button", addBtn.value);
+
+console.log("list", todoList.value);
+
+console.log("trash", deleteAllBtn.value);
